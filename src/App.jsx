75ca04944356894b6b6b076652c7f4b104fb90ab
Brescou <37625414+Brescou@users.noreply.github.com>
2022-12-07 07:23:56 -0500

    restart all with mantine

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,4 @@
-import { Route, Routes } from "react-router-dom";
-import { Navigation } from "./components/navigation/Navigation";
-import { Home } from "./pages/home/Home";
-
-
 const App = () => {
-  return (
-    <>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        {/* <Route path="/player" element={Player} /> */}
-      </Routes>
-    </>
-  );
+  return <></>;
 };
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,43 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { NextUIProvider, createTheme } from "@nextui-org/react";
-import { BrowserRouter as Router } from "react-router-dom";
-
-const theme = createTheme({
-  type: "light",
-  theme: {
-    colors: {
-      primaryLight: "#A27B5C",
-      primaryLightHover: "$green300",
-      primaryLightActive: "$green400",
-      primaryLightContrast: "$green600",
-      primary: "#4ADE7B",
-      primaryBorder: "#3F4E4F",
-      primaryBorderHover: "$green600",
-      primarySolidHover: "$green700",
-      primarySolidContrast: "$white",
-      primaryShadow: "#2C3639",
-
-      gradient:
-        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
-      link: "#DCD7C9",
-
-      myColor: "#ff4ecd",
-    },
-  },
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <NextUIProvider theme={theme}>
-      <Router>
-        <App />
-      </Router>
-    </NextUIProvider>
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
